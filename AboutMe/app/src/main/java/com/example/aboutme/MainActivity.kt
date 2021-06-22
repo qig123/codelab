e143e238cb6bc6bbbaf338c33acc92817c4dadfd
guo_qiu <qiuguoxyz2@gmail.com>
2021-06-23 00:54:33 +0800

    第一次提交

---
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("guo qiu")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName=myName;
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+        binding.nicknameText.setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding.apply {
+            myName?.nickname=nicknameEdit.text.toString()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            invalidateAll()
+        }
+        // Hide the keyboard.
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = binding.nicknameEdit
+        val doneButton = binding.doneButton
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+        // Set the focus to the edit text.
+        editText.requestFocus()
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
+    }
+
+}
